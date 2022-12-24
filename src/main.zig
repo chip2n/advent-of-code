37815f2445e4d4f0532526ff4e2229428ab6c67a
chip2n <andreas@arvidsson.io>
2022-12-24 11:33:44 +0100

    Day 1b

--- src/main.zig
+++ src/main.zig
@@ -1,33 +1,79 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const result = try day1("/Users/andreas/dev/aoc-2022/src/input-1");
-    std.log.info("{}", .{result});
+    const resultA = try day1a("/Users/andreas/dev/aoc-2022/src/input-1");
+    const resultB = try day1b("/Users/andreas/dev/aoc-2022/src/input-1");
+    std.log.info("A: {}, B: {}", .{ resultA, resultB });
 }
 
-pub fn day1(input_path: []const u8) !u32 {
+pub fn day1a(input_path: []const u8) !u32 {
     var file = try std.fs.openFileAbsolute(input_path, .{});
     var buf: [128]u8 = undefined;
     var current: u32 = 0;
     var max: u32 = 0;
     var i: usize = 0;
     while (true) : (i += 1) {
-        var line = try file.reader().readUntilDelimiterOrEof(&buf, '\n') orelse break;
-        if (line.len == 0) {
+        var line = try file.reader().readUntilDelimiterOrEof(&buf, '\n');
+        if (line == null or line.?.len == 0) {
             if (current > max) {
                 max = current;
             }
             current = 0;
-            continue;
+        } else {
+            var number = try std.fmt.parseUnsigned(u32, line.?, 10);
+            current += number;
         }
 
-        var number = try std.fmt.parseUnsigned(u32, line, 10);
-        current += number;
+        if (line == null) break;
     }
     return max;
 }
 
-test "input 1" {
-    var result = try day1("/Users/andreas/dev/aoc-2022/src/input-1-test");
+pub fn day1b(input_path: []const u8) !u32 {
+    var file = try std.fs.openFileAbsolute(input_path, .{});
+    var result: [3]u32 = [_]u32{ 0, 0, 0 };
+
+    var buf: [128]u8 = undefined;
+    var current: u32 = 0;
+    while (true) {
+        var line = try file.reader().readUntilDelimiterOrEof(&buf, '\n');
+        if (line == null or line.?.len == 0) {
+            insertResult(&result, current);
+            current = 0;
+        } else {
+            var number = try std.fmt.parseUnsigned(u32, line.?, 10);
+            current += number;
+        }
+
+        if (line == null) break;
+    }
+
+    var sum: u32 = 0;
+    for (result) |n| {
+        sum += n;
+    }
+    return sum;
+}
+
+fn insertResult(result: []u32, n: u32) void {
+    var smallestIndex: usize = 0;
+    for (result) |x, i| {
+        if (x < result[smallestIndex]) {
+            smallestIndex = i;
+        }
+    }
+
+    if (result[smallestIndex] < n) {
+        result[smallestIndex] = n;
+    }
+}
+
+test "day 1a" {
+    var result = try day1a("/Users/andreas/dev/aoc-2022/src/input-1-test");
     try std.testing.expectEqual(result, 24000);
+}
+
+test "day 1b" {
+    var result = try day1b("/Users/andreas/dev/aoc-2022/src/input-1-test");
+    try std.testing.expectEqual(result, 45000);
 }
