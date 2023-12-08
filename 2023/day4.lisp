206f81716d095b9656ea9b6a59dc579ce19010b8
chip2n <andreas@arvidsson.io>
2023-12-08 18:58:43 +0100

    Solve day 4b

--- 2023/day4.lisp
+++ 2023/day4.lisp
@@ -21,17 +21,36 @@
          (part->nums winning-part)
          (part->nums other-part))))))
 
-(defun card-points (card)
-  (let ((points 0))
+(defun card-matches (card)
+  (let ((matches nil))
     (loop for num in (cadr card)
           when (find num (car card))
-            do (setf points (if (= points 0) 1 (* points 2))))
+            do (push num matches))
+    (nreverse matches)))
+
+(defun card-points (card)
+  (let ((points 0))
+    (loop for i from 0 below (length (card-matches card))
+          do (setf points (if (= points 0) 1 (* points 2))))
     points))
 
 (defun solve-a (stream)
   (loop for card = (parse-card stream)
         while card
         sum (card-points card)))
+
+;; * Part B
+
+(defun solve-b (stream)
+  (let ((card-count-table (make-hash-table)))
+    (loop for card = (parse-card stream)
+          for n = 1 then (+ n 1)
+          while card
+          for matches = (card-matches card)
+          for card-count = (a:ensure-gethash n card-count-table 1)
+          do (loop for i from (+ n 1) upto (+ n (length matches))
+                   do (setf (gethash i card-count-table) (+ card-count (a:ensure-gethash i card-count-table 1)))))
+    (reduce #'+ (a:hash-table-values card-count-table))))
 
 ;; * Tests
 
@@ -47,3 +66,16 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 (test sample-a
   (with-input-from-string (s *sample-a*)
     (is (eql 13 (solve-a s)))))
+
+(defvar *sample-b* (string-trim '(#\Newline) "
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"))
+
+(test sample-b
+  (with-input-from-string (s *sample-b*)
+    (is (eql 30 (solve-b s)))))
