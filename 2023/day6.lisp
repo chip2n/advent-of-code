f5fa83c78c822adea7acd0a113973705872eae53
chip2n <andreas@arvidsson.io>
2023-12-09 18:12:30 +0100

    Solve day 6b

--- 2023/day6.lisp
+++ 2023/day6.lisp
@@ -30,6 +30,35 @@
                     for options = (enumerate-options duration)
                     collect (length (remove-if (lambda (d) (>= distance d)) options)))))))
 
+;; * Part B
+
+(defun read-numbers-kerning (stream)
+  (read-until stream (lambda (c) (char= c #\Colon)))
+  (read-char stream)
+  (->> (read-line stream)
+       (str:split " ")
+       (apply #'concatenate 'string)
+       (parse-integer)))
+
+(defun count-options (duration record)
+  (let* ((min (loop for i from 0 upto duration
+                    for distance = (* i (- duration i))
+                    when (< record distance)
+                      return i))
+         (max (loop for i from min upto duration
+                    for distance = (* i (- duration i))
+                    when (> record distance)
+                      return (1- i))))
+    (if (and min max)
+        (1+ (- max min))
+        0)))
+
+(defun solve-b (stream)
+  (let ((str:*omit-nulls* t))
+    (let* ((duration (read-numbers-kerning stream))
+           (record (read-numbers-kerning stream)))
+      (count-options duration record))))
+
 ;; * Tests
 
 (defvar *sample* (string-trim '(#\Newline) "
@@ -40,3 +69,7 @@ Distance:  9  40  200
 (test sample-a
   (with-input-from-string (s *sample*)
     (is (eql 288 (solve-a s)))))
+
+(test sample-b
+  (with-input-from-string (s *sample*)
+    (is (eql 71503 (solve-b s)))))
